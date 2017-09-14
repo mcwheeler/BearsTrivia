326edae4832ab172aa97a0e5897f490be38a6fd8
Michael Wheeler <mcwheeler@solidconsultants.com>
2017-09-13 19:08:46 -0500

    Added Index and question files

---
+++ src/question.js
@@ -0,0 +1,103 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'As of the 2016 season, how many league championships have the Bears won?': [
+                '9',
+                '6',
+                '7',
+                '8',
+                '10',
+                '5',
+            ],
+        },
+        {
+            'What year was the team established?': [
+                '1919',
+                '1918',
+                '1917',
+                '1920',
+                '1916',
+                '1921',
+                '1915',
+            ],
+        },
+        {
+            'What Illinois city was the team established in?': [
+                'Decatur',
+                'Chicago',
+                'Lake Forest',
+                'Bourbonnais',
+            ],
+        },
+        {
+            'Who was not a coach of the Bears?': [
+                'Tony Dungy',
+                'George Halas',
+                'Lovie Smith',
+                'Mike Ditka',
+            ],
+        },
+        {
+            'What stadium did the Bears play in during the 2002 season?': [
+                'Memorial Stadium',
+                'Soldier Field',
+                'Wrigley Field',
+                'Illinios Stadium',
+            ],
+        },
+        {
+            'Who was not a popular linebacker for the Bears?': [
+                'Ray Nitschke',
+                'Dick Butkus',
+                'Mike Singletary',
+                'Brian Urlacher',
+            ],
+        },
+        {
+            'What Super Bowl did the Bears win?': [
+                'Super Bowl 20',
+                'Super Bowl 19',
+                'Super Bowl 21',
+                'Super Bowl 22',
+            ],
+        },
+        {
+            'What Super Bowl did the Bears lose?': [
+                'Super Bowl 31',
+                'Super Bowl 20',
+                'Super Bowl 30',
+                'Super Bowl 29',
+            ],
+        },
+        {
+            'Walter Payton is the all-time leading rusher in Bears history, who is number two?': [
+                'Matt Forte',
+                'Neal Anderson',
+                'Gale Sayers',
+                'Rick Casares',
+            ],
+        },
+        {
+            'What season did Jay Cutler pass Sid Luckman to become the Bears all-time leading passer?': [
+                '2013 Season',
+                '2012 Season',
+                '2014 Season',
+                '2015 Season',
+            ],
+        },
+        {
+            'How many hall of famers made their primary contribution with the Bears?': [
+                '27',
+                '34',
+                '28',
+                '33',
+            ],
+        },
+    ],
+};
